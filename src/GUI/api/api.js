e263581db6eec1a40d977aabeb95bc9c8f3f4c56
sergism <sergism@gmail.com>
2023-04-30 12:33:41 +0200

    working on the api

--- src/GUI/api/api.js
+++ src/GUI/api/api.js
@@ -1,7 +1,19 @@
-//this is the api.js file that contains all the api calls
-//the api will be available in the whole app and served to https://api.seigr.network
-//the api key is freekey
-//we have the different api calls in different files in the api folder
-
 import axios from 'axios';
+import fetch from 'node-fetch';
+
+import { blocks, blocksByDate, blocksByDateRange, blocksByUser, blocksByUserAndDate } from './blocks';
+import { contracts, contractsByDate, contractsByDateRange, contractsByUser, contractsByUserAndDate } from './contracts';
+import { tokenTransfers, tokenTransfersByDate, tokenTransfersByDateRange, tokenTransfersByUser, tokenTransfersByUserAndDate } from './tokenTransfers';
+import { tokenBalances, tokenBalancesByDate, tokenBalancesByDateRange, tokenBalancesByUser, tokenBalancesByUserAndDate } from './tokenBalances';
+import { tokens, tokensByContract, tokensByContractAndDate, tokensByContractAndDateRange, tokensByContractAndUser, tokensByContractAndUserAndDate, tokensByContractAndUserAndDateRange, tokensByContractAndCategory, tokensByContractAndCategoryAndDate, tokensByContractAndCategoryAndDateRange, tokensByContractAndCategoryAndUser, tokensByContractAndCategoryAndUserAndDate, tokensByContractAndCategoryAndUserAndDateRange, tokensByCategoryAndDate } from './tokens';
+import { logs, logsByDate, logsByDateRange, logsByUser, logsByUserAndDate } from './logs';
+import { users, usersByDate, usersByDateRange } from './users';
+import { categories, categoriesByDate, categoriesByDateRange } from './categories';
+import { contractsByCategory, contractsByCategoryAndDate, contractsByCategoryAndDateRange } from './contractsByCategory';
+import { tokenTransfersByCategory, tokenTransfersByCategoryAndDate, tokenTransfersByCategoryAndDateRange } from './tokenTransfersByCategory';
+import { tokenBalancesByCategory, tokenBalancesByCategoryAndDate, tokenBalancesByCategoryAndDateRange } from './tokenBalancesByCategory';
+import { tokensByCategory, tokensByCategoryAndDate, tokensByCategoryAndDateRange } from './tokensByCategory';
+import { logs, logsByDate, logsByDateRange, logsByUser, logsByUserAndDate } from './logs';
+import { traces, tracesByDate, tracesByDateRange, tracesByUser, tracesByUserAndDate } from './traces';
+
 
