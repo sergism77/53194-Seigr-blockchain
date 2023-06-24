ac7521c49a89d0b9a42ee6626160ea79d3d4d1dc
sergism <sergism@gmail.com>
2023-06-24 22:14:19 +0200

    peerPool

--- src/SeigrBlockchain/peerPoolDirectory.ts
+++ src/SeigrBlockchain/peerPoolDirectory.ts
@@ -1,277 +1,258 @@
-'use strict';
-
 class PeerPoolDirectory {
-    private peerPoolDirectory: any[];
-  
-    constructor() {
-      this.peerPoolDirectory = [];
-    }
-  
-    public addPeer(peer: any): void {
-      this.peerPoolDirectory.push(peer);
-    }
-  
-    public removePeer(peer: any): void {
-      this.peerPoolDirectory.splice(this.peerPoolDirectory.indexOf(peer), 1);
-    }
-  
-    public getPeerPoolDirectory(): any[] {
-      return this.peerPoolDirectory;
-    }
-  
-    public setPeerPoolDirectory(peerPoolDirectory: any[]): any[] {
-      this.peerPoolDirectory = peerPoolDirectory;
-      return this.peerPoolDirectory;
-    }
+  private peerPoolDirectory: any[];
 
-    public getPeerPoolDirectoryLength(): number {
-      return this.peerPoolDirectory.length;
-    }
+  constructor() {
+    this.peerPoolDirectory = [];
+  }
 
-    public getPeerPoolDirectoryElement(index: number): any {
-      return this.peerPoolDirectory[index];
-    }
+  public addPeer(peer: any): void {
+    this.peerPoolDirectory.push(peer);
+  }
 
-    public setPeerPoolDirectoryElement(index: number, peer: any): void {
-      this.peerPoolDirectory[index] = peer;
+  public removePeer(peer: any): void {
+    const index = this.peerPoolDirectory.indexOf(peer);
+    if (index !== -1) {
+      this.peerPoolDirectory.splice(index, 1);
     }
+  }
 
-    public getPeerPoolDirectoryElementByPeer(peer: any): any {
-      return this.peerPoolDirectory[this.peerPoolDirectory.indexOf(peer)];
+  public getPeerPoolDirectory(): any[] {
+    return this.peerPoolDirectory;
+  }
+
+  public setPeerPoolDirectory(peerPoolDirectory: any[]): void {
+    this.peerPoolDirectory = peerPoolDirectory;
+  }
+
+  public getPeerPoolDirectoryLength(): number {
+    return this.peerPoolDirectory.length;
+  }
+
+  public getPeerPoolDirectoryElement(index: number): any {
+    return this.peerPoolDirectory[index];
+  }
+
+  public setPeerPoolDirectoryElement(index: number, peer: any): void {
+    this.peerPoolDirectory[index] = peer;
+  }
+
+  public getPeerPoolDirectoryElementByPeer(peer: any): any {
+    return this.peerPoolDirectory.find((p) => p === peer);
+  }
+
+  public setPeerPoolDirectoryElementByPeer(peer: any, newPeer: any): void {
+    const index = this.peerPoolDirectory.indexOf(peer);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
     }
+  }
 
-    public setPeerPoolDirectoryElementByPeer(peer: any, newPeer: any): void {
-      this.peerPoolDirectory[this.peerPoolDirectory.indexOf(peer)] = newPeer;
+  public getPeerPoolDirectoryElementByPeerId(peerId: string): any {
+    return this.peerPoolDirectory.find((p) => p.getPeerId() === peerId);
+  }
+
+  public setPeerPoolDirectoryElementByPeerId(peerId: string, newPeer: any): void {
+    const index = this.peerPoolDirectory.findIndex((p) => p.getPeerId() === peerId);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
     }
+  }
 
-    public getPeerPoolDirectoryElementByPeerId(peerId: string): any {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerId() === peerId) {
-          return this.peerPoolDirectory[i];
-        }
+  public getPeerPoolDirectoryElementByPeerIp(peerIp: string): any {
+    return this.peerPoolDirectory.find((p) => p.getPeerIp() === peerIp);
+  }
+
+  public setPeerPoolDirectoryElementByPeerIp(peerIp: string, newPeer: any): void {
+    const index = this.peerPoolDirectory.findIndex((p) => p.getPeerIp() === peerIp);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
+    }
+  }
+
+  public getPeerPoolDirectoryElementByPeerPort(peerPort: number): any {
+    return this.peerPoolDirectory.find((p) => p.getPeerPort() === peerPort);
+  }
+
+  public setPeerPoolDirectoryElementByPeerPort(peerPort: number, newPeer: any): void {
+    const index = this.peerPoolDirectory.findIndex((p) => p.getPeerPort() === peerPort);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
+    }
+  }
+
+  public getPeerPoolDirectoryElementByPeerSocket(peerSocket: any): any {
+    return this.peerPoolDirectory.find((p) => p.getPeerSocket() === peerSocket);
+  }
+
+  public setPeerPoolDirectoryElementByPeerSocket(peerSocket: any, newPeer: any): void {
+    const index = this.peerPoolDirectory.findIndex((p) => p.getPeerSocket() === peerSocket);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
+    }
+  }
+
+  public getPeerPoolDirectoryElementByPeerSocketId(peerSocketId: string): any {
+    return this.peerPoolDirectory.find((p) => p.getPeerSocketId() === peerSocketId);
+  }
+
+  public setPeerPoolDirectoryElementByPeerSocketId(peerSocketId: string, newPeer: any): void {
+    const index = this.peerPoolDirectory.findIndex((p) => p.getPeerSocketId() === peerSocketId);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
+    }
+  }
+
+  public getPeerPoolDirectoryElementByPeerSocketIp(peerSocketIp: string): any {
+    return this.peerPoolDirectory.find((p) => p.getPeerSocketIp() === peerSocketIp);
+  }
+
+  public setPeerPoolDirectoryElementByPeerSocketIp(peerSocketIp: string, newPeer: any): void {
+    const index = this.peerPoolDirectory.findIndex((p) => p.getPeerSocketIp() === peerSocketIp);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
+    }
+  }
+
+  public getPeerPoolDirectoryElementByPeerSocketPort(peerSocketPort: number): any {
+    return this.peerPoolDirectory.find((p) => p.getPeerSocketPort() === peerSocketPort);
+  }
+
+  public setPeerPoolDirectoryElementByPeerSocketPort(peerSocketPort: number, newPeer: any): void {
+    const index = this.peerPoolDirectory.findIndex((p) => p.getPeerSocketPort() === peerSocketPort);
+    if (index !== -1) {
+      this.peerPoolDirectory[index] = newPeer;
+    }
+  }
+
+  public broadcast(message: any): void {
+    for (let i = 0; i < this.peerPoolDirectory.length; i++) {
+      const peer = this.peerPoolDirectory[i];
+      if (peer && typeof peer.send === "function") {
+        peer.send(message);
       }
     }
+  }
 
-    public setPeerPoolDirectoryElementByPeerId(peerId: string, newPeer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerId() === peerId) {
-          this.peerPoolDirectory[i] = newPeer;
-        }
+  public broadcastExcept(message: any, peer: any): void {
+    for (let i = 0; i < this.peerPoolDirectory.length; i++) {
+      const otherPeer = this.peerPoolDirectory[i];
+      if (otherPeer !== peer && typeof otherPeer.send === "function") {
+        otherPeer.send(message);
       }
     }
+  }
 
-    public getPeerPoolDirectoryElementByPeerIp(peerIp: string): any {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerIp() === peerIp) {
-          return this.peerPoolDirectory[i];
-        }
+  public broadcastTo(peerPool: any[], message: any): void {
+    for (let i = 0; i < peerPool.length; i++) {
+      const peer = peerPool[i];
+      if (peer && typeof peer.send === "function") {
+        peer.send(message);
       }
     }
+  }
 
-    public setPeerPoolDirectoryElementByPeerIp(peerIp: string, newPeer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerIp() === peerIp) {
-          this.peerPoolDirectory[i] = newPeer;
-        }
+  public broadcastToExcept(peerPool: any[], message: any, peer: any): void {
+    for (let i = 0; i < peerPool.length; i++) {
+      const otherPeer = peerPool[i];
+      if (otherPeer !== peer && typeof otherPeer.send === "function") {
+        otherPeer.send(message);
       }
     }
+  }
 
-    public getPeerPoolDirectoryElementByPeerPort(peerPort: number): any {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerPort() === peerPort) {
-          return this.peerPoolDirectory[i];
-        }
-      }
+  public receive(message: any, peer: any): void {
+    if (message.type === "request" && message.request === "peerPoolDirectory") {
+      peer.send({
+        type: "response",
+        response: "peerPoolDirectory",
+        peerPoolDirectory: this.peerPoolDirectory,
+      });
     }
+  }
 
-    public setPeerPoolDirectoryElementByPeerPort(peerPort: number, newPeer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerPort() === peerPort) {
-          this.peerPoolDirectory[i] = newPeer;
-        }
-      }
+  public receiveFrom(message: any, peerPool: any[], peer: any): void {
+    if (message.type === "request" && message.request === "peerPoolDirectory") {
+      peer.send({
+        type: "response",
+        response: "peerPoolDirectory",
+        peerPoolDirectory: this.peerPoolDirectory,
+      });
     }
+  }
 
-    public getPeerPoolDirectoryElementByPeerSocket(peerSocket: any): any {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocket() === peerSocket) {
-          return this.peerPoolDirectory[i];
-        }
-      }
+  public receiveFromExcept(message: any, peerPool: any[], peer: any): void {
+    if (message.type === "request" && message.request === "peerPoolDirectory") {
+      peer.send({
+        type: "response",
+        response: "peerPoolDirectory",
+        peerPoolDirectory: this.peerPoolDirectory,
+      });
     }
+  }
 
-    public setPeerPoolDirectoryElementByPeerSocket(peerSocket: any, newPeer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocket() === peerSocket) {
-          this.peerPoolDirectory[i] = newPeer;
-        }
-      }
-    }
-
-    public getPeerPoolDirectoryElementByPeerSocketId(peerSocketId: string): any {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocketId() === peerSocketId) {
-          return this.peerPoolDirectory[i];
-        }
-      }
-    }
-
-    public setPeerPoolDirectoryElementByPeerSocketId(peerSocketId: string, newPeer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocketId() === peerSocketId) {
-          this.peerPoolDirectory[i] = newPeer;
-        }
-      }
-    }
-
-    public getPeerPoolDirectoryElementByPeerSocketIp(peerSocketIp: string): any {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocketIp() === peerSocketIp) {
-          return this.peerPoolDirectory[i];
-        }
-      }
-    }
-
-    public setPeerPoolDirectoryElementByPeerSocketIp(peerSocketIp: string, newPeer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocketIp() === peerSocketIp) {
-          this.peerPoolDirectory[i] = newPeer;
-        }
-      }
-    }
-
-    public getPeerPoolDirectoryElementByPeerSocketPort(peerSocketPort: number): any {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocketPort() === peerSocketPort) {
-          return this.peerPoolDirectory[i];
-        }
-      }
-    }
-
-    public setPeerPoolDirectoryElementByPeerSocketPort(peerSocketPort: number, newPeer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i].getPeerSocketPort() === peerSocketPort) {
-          this.peerPoolDirectory[i] = newPeer;
-        }
-      }
-    }
-
-    public broadcast (message: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        this.peerPoolDirectory[i].send(message);
-      }
-    }
-
-    public broadcastExcept (message: any, peer: any): void {
-      for (let i = 0; i < this.peerPoolDirectory.length; i++) {
-        if (this.peerPoolDirectory[i] !== peer) {
-          this.peerPoolDirectory[i].send(message);
-        }
-      }
-    }
-
-    public broadcastTo (message: any, peerPool: any): void {
+  public receiveFromAll(message: any, peerPool: any[]): void {
+    if (message.type === "request" && message.request === "peerPoolDirectory") {
       for (let i = 0; i < peerPool.length; i++) {
-        peerPool[i].send(message);
+        const peer = peerPool[i];
+        if (peer && typeof peer.send === "function") {
+          peer.send({
+            type: "response",
+            response: "peerPoolDirectory",
+            peerPoolDirectory: this.peerPoolDirectory,
+          });
+        }
       }
     }
+  }
 
-    public broadcastToExcept (message: any, peerPool: any, peer: any): void {
+  public receiveFromAllExcept(message: any, peerPool: any[], peer: any): void {
+    if (message.type === "request" && message.request === "peerPoolDirectory") {
       for (let i = 0; i < peerPool.length; i++) {
-        if (peerPool[i] !== peer) {
-          peerPool[i].send(message);
-        }
-      }
-    }
-
-    public receive (message: any, peer: any): void {
-      if (message.type === 'request') {
-        if (message.request === 'peerPoolDirectory') {
-          peer.send({
-            type: 'response',
-            response: 'peerPoolDirectory',
-            peerPoolDirectory: this.peerPoolDirectory
+        const otherPeer = peerPool[i];
+        if (otherPeer !== peer && typeof otherPeer.send === "function") {
+          otherPeer.send({
+            type: "response",
+            response: "peerPoolDirectory",
+            peerPoolDirectory: this.peerPoolDirectory,
           });
         }
       }
     }
+  }
 
-    public receiveFrom (message: any, peerPool: any, peer: any): void {
-      if (message.type === 'request') {
-        if (message.request === 'peerPoolDirectory') {
-          peer.send({
-            type: 'response',
-            response: 'peerPoolDirectory',
-            peerPoolDirectory: this.peerPoolDirectory
-          });
-        }
-      }
+  public receiveFromAllTo(message: any, peerPool: any[], peer: any): void {
+    if (message.type === "request" && message.request === "peerPoolDirectory") {
+      peer.send({
+        type: "response",
+        response: "peerPoolDirectory",
+        peerPoolDirectory: this.peerPoolDirectory,
+      });
     }
+  }
 
-    public receiveFromExcept (message: any, peerPool: any, peer: any): void {
-      if (message.type === 'request') {
-        if (message.request === 'peerPoolDirectory') {
-          peer.send({
-            type: 'response',
-            response: 'peerPoolDirectory',
-            peerPoolDirectory: this.peerPoolDirectory
-          });
-        }
-      }
-    }
-
-    public receiveFromAll (message: any, peerPool: any): void {
-      if (message.type === 'request') {
-        if (message.request === 'peerPoolDirectory') {
-          peerPool.send({
-            type: 'response',
-            response: 'peerPoolDirectory',
-            peerPoolDirectory: this.peerPoolDirectory
-          });
-        }
-      }
-    }
-
-    public receiveFromAllExcept (message: any, peerPool: any, peer: any): void {
-      if (message.type === 'request') {
-        if (message.request === 'peerPoolDirectory') {
-          peerPool.send({
-            type: 'response',
-            response: 'peerPoolDirectory',
-            peerPoolDirectory: this.peerPoolDirectory
-          });
-        }
-      }
-    }
-
-    public receiveFromAllTo (message: any, peerPool: any, peer: any): void {
-      if (message.type === 'request') {
-        if (message.request === 'peerPoolDirectory') {
-          peer.send({
-            type: 'response',
-            response: 'peerPoolDirectory',
-            peerPoolDirectory: this.peerPoolDirectory
-          });
-        }
-      }
-    }
-
-    public send (message: any, peer: any): void {
+  public send(message: any, peer: any): void {
+    if (peer && typeof peer.send === "function") {
       peer.send(message);
     }
+  }
 
-    public sendTo (message: any, peerPool: any, peer: any): void {
+  public sendTo(peerPool: any[], message: any, peer: any): void {
+    if (peer && typeof peer.send === "function") {
       peer.send(message);
     }
+  }
 
-    public disconnect (peer: any): void {
+  public disconnect(peer: any): void {
+    if (peer && typeof peer.disconnect === "function") {
       peer.disconnect();
     }
+  }
 
-    public connect (peer: any): void {
+  public connect(peer: any): void {
+    if (peer && typeof peer.connect === "function") {
       peer.connect();
     }
-
   }
-  
-  export { PeerPoolDirectory };
+}
+
+export { PeerPoolDirectory };
