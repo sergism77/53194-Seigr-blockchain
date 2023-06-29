8b4e11a9e397cab09625058083d5af4d823e9f2e
sergism <sergism@gmail.com>
2023-06-29 12:39:12 +0200

    connect to peers

--- src/SeigrBlockchain/connectToPeers.ts
+++ src/SeigrBlockchain/connectToPeers.ts
@@ -1,3 +1,5 @@
+/* The class ConnectionError extends the Error class and represents an error that occurs when a
+connection to a peer fails. */
 class ConnectionError extends Error {
     constructor(peer: string, message: string) {
       super(`Failed to connect to peer ${peer}: ${message}`);
@@ -5,6 +7,8 @@ class ConnectionError extends Error {
     }
   }
   
+/* The `ConnectToPeers` class is responsible for connecting to a list of peers using WebSockets. Here
+is a breakdown of what each part of the class does: */
   class ConnectToPeers {
     sockets: WebSocket[];
   
@@ -12,11 +16,22 @@ class ConnectionError extends Error {
       this.sockets = [];
     }
   
+/**
+ * The function `isValidUrl` checks if a given string is a valid URL.
+ * @param {string} url - A string representing a URL.
+ * @returns The function `isValidUrl` returns a boolean value.
+ */
     isValidUrl(url: string): boolean {
       const urlRegex = /^(ftp|http|https|wss):\/\/[^ "]+$/;
       return urlRegex.test(url);
     }
   
+    /**
+     * The `connectSocket` function sets up event listeners for a WebSocket connection, handling
+     * incoming messages and closing events.
+     * @param {WebSocket} socket - The `socket` parameter is of type `WebSocket`. It represents the
+     * WebSocket connection that you want to connect to.
+     */
     connectSocket(socket: WebSocket): void {
       const messageListener = (event: MessageEvent): void => {
         const socket = event.currentTarget as WebSocket;
@@ -35,38 +50,47 @@ class ConnectionError extends Error {
       const closeListener = (): void => {
         console.log('Socket closed');
         this.sockets = this.sockets.filter(s => s !== socket);
-        socket.removeEventListener('message', messageListener);
-        socket.removeEventListener('close', closeListener);
       };
   
       socket.addEventListener('message', messageListener);
       socket.addEventListener('close', closeListener);
     }
   
+/**
+ * The function creates a WebSocket connection to a specified peer and returns a promise that resolves
+ * with the WebSocket object.
+ * @param {string} peer - The `peer` parameter is a string that represents the URL of the WebSocket
+ * server that the client wants to connect to.
+ * @returns A Promise object is being returned.
+ */
     private createWebSocket(peer: string): Promise<WebSocket> {
       return new Promise((resolve, reject) => {
         const socket = new WebSocket(peer);
         socket.addEventListener('open', () => resolve(socket));
         socket.addEventListener('error', (error: Event) => {
-          const errorMessage = (error as unknown) instanceof Error ? (error as Error).message : 'Unknown error occurred';
+          const errorMessage = (error instanceof Error) ? error.message : 'Unknown error occurred';
           reject(new ConnectionError(peer, errorMessage));
         });
       });
     }
   
-    async connectToPeers() {
+   /**
+    * The function connects to a list of peers by creating websockets and handling any connection
+    * errors.
+    */
+    async connectToPeers(): Promise<void> {
       for (const peer of this.peers) {
         try {
           if (!this.isValidUrl(peer)) {
             throw new Error(`Invalid peer URL: ${peer}`);
           }
-          
+  
           const socket = await this.createWebSocket(peer);
           this.connectSocket(socket);
           console.log(`Connected to peer: ${peer}`);
         } catch (error) {
           if (error instanceof ConnectionError) {
-            console.error(`Failed to connect to peer ${peer}: ${(error as Error).message}`);
+            console.error(`Failed to connect to peer ${peer}: ${error.message}`);
           } else {
             console.error(error);
           }
