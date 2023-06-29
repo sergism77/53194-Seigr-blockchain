ae2f19971fa8618bafaa665b8afb5e56239cb57b
sergism <sergism@gmail.com>
2023-06-29 11:39:57 +0200

    quadratic voting system implemented

--- src/SeigrBlockchain/quadraticVoting.ts
+++ src/SeigrBlockchain/quadraticVoting.ts
@@ -1,4 +1,7 @@
+import { get } from "lodash";
+
 interface VotingProposal {
+  proposalId: number;
   votes: number;
   voters: Set<number>;
 }
@@ -25,93 +28,28 @@ class ProposalNotFoundError extends Error {
 /**
  * QuadraticVoting class represents a quadratic voting system for proposals.
  */
-export class QuadraticVoting {
-  private readonly votingProposals: { [key: number]: VotingProposal };
+class QuadraticVoting {
+  private votingProposals: { [key: number]: VotingProposal };
 
-  /**
-   * Creates an instance of QuadraticVoting.
-   */
-  constructor() {
-    this.votingProposals = {};
+  constructor(votingProposals: { [key: number]: VotingProposal }) {
+    this.votingProposals = votingProposals;
   }
 
-  /**
-   * Creates a quadratic voting proposal with the given proposalId.
-   * @param proposalId - The ID of the proposal.
-   * @throws {ProposalAlreadyExistsError} If a proposal with the same ID already exists.
-   */
-  createQuadraticVotingProposal(proposalId: number): void {
-    if (this.votingProposals.hasOwnProperty(proposalId)) {
-      throw new ProposalAlreadyExistsError();
-    }
-
-    this.votingProposals[proposalId] = { votes: 0, voters: new Set<number>() };
-  }
-
-  /**
-   * Votes on a quadratic voting proposal with the given proposalId using the specified votingPower.
-   * @param proposalId - The ID of the proposal.
-   * @param votingPower - The voting power of the voter.
-   * @throws {ProposalNotFoundError} If the proposal with the given ID doesn't exist.
-   */
-  voteOnQuadraticVotingProposal(proposalId: number, votingPower: number): void {
-    if (!this.votingProposals.hasOwnProperty(proposalId)) {
-      throw new ProposalNotFoundError(proposalId);
-    }
-
-    const proposal = this.votingProposals[proposalId];
-
-    if (proposal.voters.has(votingPower)) {
-      throw new Error("Same voter cannot vote multiple times for the same proposal");
-    }
-
-    proposal.votes += votingPower ** 2;
-    proposal.voters.add(votingPower);
-  }
-
-  /**
-   * Retrieves the total votes for a quadratic voting proposal with the given proposalId.
-   * @param proposalId - The ID of the proposal.
-   * @returns The total votes for the proposal.
-   * @throws {ProposalNotFoundError} If the proposal with the given ID doesn't exist.
-   */
-  tallyQuadraticVotingVotes(proposalId: number): number {
-    if (!this.votingProposals.hasOwnProperty(proposalId)) {
-      throw new ProposalNotFoundError(proposalId);
-    }
-
-    return this.votingProposals[proposalId].votes;
-  }
-
-  /**
-   * Retrieves all the quadratic voting proposals and their respective vote counts.
-   * @returns A map of proposalId to VotingProposal.
-   */
   getAllQuadraticVotingProposals(): { [key: number]: VotingProposal } {
     if (Object.keys(this.votingProposals).length === 0) {
       throw new Error("No quadratic voting proposals available.");
     }
-  
+
     return this.votingProposals;
   }
 
-  /**
-   * Removes a quadratic voting proposal with the given proposalId.
-   * @param proposalId - The ID of the proposal.
-   * @returns true if the proposal was successfully removed, false if the proposal with the given ID doesn't exist.
-   * @throws {ProposalNotFoundError} If the proposalId parameter is not a valid number or if the proposal with the given ID doesn't exist.
-   */
-     
-
   removeQuadraticVotingProposal(proposalId: number): boolean {
-    if (typeof proposalId !== "number" || !Number.isInteger(proposalId) || proposalId <= 0) {
-      throw new Error("Invalid proposalId. Expected a positive integer.");
-    }
-  
+    this.validateProposalId(proposalId);
+
     if (!this.votingProposals.hasOwnProperty(proposalId)) {
       return false; // Proposal doesn't exist
     }
-  
+
     try {
       delete this.votingProposals[proposalId];
       return true; // Proposal was successfully removed
@@ -121,30 +59,94 @@ export class QuadraticVoting {
     }
   }
 
-  /**
-   * Executes the quadratic voting proposal with the given proposalId.
-   * @param proposalId - The ID of the proposal.
-   * @throws {ProposalNotFoundError} If the proposalId parameter is not a valid number or if the proposal with the given ID doesn't exist.
-   * @throws {Error} If the execution logic is not implemented.
-   */
   executeQuadraticVotingProposal(proposalId: number): void {
-    if (typeof proposalId !== "number" || !Number.isInteger(proposalId) || proposalId <= 0) {
-      throw new Error("Invalid proposalId. Expected a positive integer.");
-    }
-  
+    this.validateProposalId(proposalId);
+
     if (!this.votingProposals.hasOwnProperty(proposalId)) {
       throw new ProposalNotFoundError(proposalId);
     }
 
     // Add implementation logic here
-    // use the tallyQuadraticVotingVotes function to get the total votes for the proposal and then use the getAllQuadraticVotingProposals function to get the proposal with the highest votes.
     this.tallyQuadraticVotingVotes(proposalId);
-    this.getAllQuadraticVotingProposals();
-    
-    // Then use the removeQuadraticVotingProposal function to remove the proposal with the highest votes.
-    this.removeQuadraticVotingProposal(proposalId);
+    const proposals = this.getAllQuadraticVotingProposals();
+    const proposalWithHighestVotes = this.getProposalWithHighestVotes(proposals);
+    this.removeQuadraticVotingProposal(proposalWithHighestVotes.proposalId);
 
-  // Remove the throw statement once the implementation is complete
-  throw new Error("Execution logic not implemented");
+    // Remove the throw statement once the implementation is complete
+    throw new Error("Execution logic not fully implemented");
+  }
+
+  private validateProposalId(proposalId: number): void {
+    if (typeof proposalId !== "number" || !Number.isInteger(proposalId) || proposalId <= 0) {
+      throw new Error("Invalid proposalId. Expected a positive integer.");
+    }
+  }
+
+  private getProposalWithHighestVotes(proposals: { [key: number]: VotingProposal }): VotingProposal {
+    let highestVotes = 0;
+    let highestVotesProposal: VotingProposal | null = null;
+
+    for (const proposalId in proposals) {
+      const proposal = proposals[proposalId];
+      if (proposal.votes > highestVotes) {
+        highestVotes = proposal.votes;
+        highestVotesProposal = proposal;
+      }
+    }
+
+    if (highestVotesProposal === null) {
+      throw new Error("No proposal found with the highest votes.");
+    }
+
+    return highestVotesProposal;
+  }
+
+  private tallyQuadraticVotingVotes(proposalId: number): number {
+    const proposal = this.votingProposals[proposalId];
+
+    if (!proposal) {
+      throw new ProposalNotFoundError(proposalId);
+    }
+
+    // Implementation logic to tally votes for a proposal
+    // Create the tallying logic for our quadratic voting here
+    //we need to get the number of votes and the number of voters from the proposal
+    getNumberOfVoters(proposal);
+    getNumberOfVotes(proposal);
+    //we need to get the square root of the number of voters
+    getSquareRootOfVoters(proposal);
+    //we need to multiply the square root of the number of voters by the number of votes
+    multiplySquareRootByVotes(proposal);
+    //we need to return the result of the multiplication
+    const resultOfMultiplication = getSquareRootOfVoters(proposal) * getNumberOfVotes(proposal);
+
+    return resultOfMultiplication;
+
+
+  }
 }
+function getNumberOfVoters(proposal: VotingProposal) {
+  //we need to get the number of voters from the proposal
+  const numberOfVoters = proposal.voters.size;
+  return numberOfVoters;
+  
 }
+
+function getNumberOfVotes(proposal: VotingProposal) {
+  //we need to get the number of votes from the proposal
+  const numberOfVotes = proposal.votes;
+  return numberOfVotes;
+}
+
+function getSquareRootOfVoters(proposal: VotingProposal) {
+  //we need to get the square root of the number of voters
+  const squareRootOfVoters = Math.sqrt(proposal.voters.size);
+  return squareRootOfVoters;
+}
+
+function multiplySquareRootByVotes(proposal: VotingProposal) {
+  //we need to multiply the square root of the number of voters by the number of votes
+  const resultOfMultiplication = getSquareRootOfVoters(proposal) * getNumberOfVotes(proposal);
+  return resultOfMultiplication;
+}
+
